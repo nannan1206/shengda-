63c5725b45dbb2c6ef5d97ab3fc454d5cc2dbd4a
DESKTOP-VKO2F9R\nanNAN <1419133864@qq.com>
2017-12-29 17:35:23 +0800

    微信订单查询

---
+++ orderSelect/js/index.js
@@ -0,0 +1,9 @@
+//页面宽度自适应
+function bodyScale(){
+			var devicewidth=document.documentElement.clientWidth;
+			var scale=devicewidth/640;
+			document.body.style.zoom=scale;
+		}
+		window.onload=window.onresize=function(){
+			bodyScale();
+		}
