c720b5b159eeaa2ff43df776fae480aa3cb9c1d4
张销男 <1419133864@qq.com>
2018-11-06 11:19:06 +0800

    大崔网站改版

---
+++ 前端网站/daCui/js/common.js
@@ -0,0 +1,17 @@
+//头部弹出框
+//气泡弹出框
+			var appTop= new Vue({
+				el:'#app0',
+				data:{
+					
+				}
+			})
+//var Main = {
+//	data() {
+//		return {};
+//	}
+//};
+//var Ctor = Vue.extend(Main)
+//new Ctor().$mount('#app0')
+	//页脚
+new Vue().$mount('#appfoot')
