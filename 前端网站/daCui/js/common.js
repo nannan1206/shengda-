77871756d322533e00cbe627e0fe357fe2e96ff3
张销男 <1419133864@qq.com>
2019-05-17 16:28:28 +0800

    文件整理更新

--- 前端网站/daCui/js/common.js
+++ 前端网站/daCui/js/common.js
@@ -1,17 +1,33 @@
 //头部弹出框
+// fade out
+function fadeOut(el) {
+	el.style.opacity = 1;
+	(function fade() {
+		if ((el.style.opacity -= .1) < 0) {
+			el.style.display = 'none';
+		} else {
+			requestAnimationFrame(fade);
+		}
+	})();
+}
+var el = document.querySelector('.changPayArea');
+el.addEventListener('click', function(e) {
+	fadeOut(el);
+});
 //气泡弹出框
-			var appTop= new Vue({
-				el:'#app0',
-				data:{
-					
-				}
-			})
-//var Main = {
-//	data() {
-//		return {};
-//	}
-//};
-//var Ctor = Vue.extend(Main)
-//new Ctor().$mount('#app0')
+//var appTop = new Vue({
+//		el: '#app0',
+//		data: {
+//
+//		}
+//	})
+	//var Main = {
+	//	data() {
+	//		return {};
+	//	}
+	//};
+	//var Ctor = Vue.extend(Main)
+	//new Ctor().$mount('#app0')
 	//页脚
-new Vue().$mount('#appfoot')
+//new Vue().$mount('#appfoot')
+
